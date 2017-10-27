449be9f74d0a898ebd47af6f4f773996c40aa449
Bryan Hughes <bryanhug@umich.edu>
2017-10-27 14:20:09 -0400

    initial commit

---
+++ rpn.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import operator
+ops = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
+def calculate(arg):
+    stack =  list()
+    for token in arg.split():
+        try:
+            stack.append(int(token))
+        except ValueError:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            function = ops[token]
+            result = function(arg1,arg2)
+            stack.append(result)
+    #print(stack)
+    return stack.pop()
+
+def main():
+    while True:
+        calculate(input("rpn calc> "))
+
+if __name__ == '__main__':
+    main()

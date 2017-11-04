ec4f1fecc8c7d38f99b8def715fed7a3e492ebca
Bryan Hughes <bryanhug@umich.edu>
2017-11-04 15:35:48 -0400

    got alot of stuff working

--- rpn.py
+++ rpn.py
@@ -3,20 +3,38 @@ import operator
 ops = {
     '+': operator.add,
     '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
 }
 
 def calculate(arg):
+    try:
+        arg.split
+    except KeyboardIn
+
     stack =  list()
     for token in arg.split():
         try:
             stack.append(int(token))
         except ValueError:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            function = ops[token]
-            result = function(arg1,arg2)
-            stack.append(result)
-    #print(stack)
+            try :
+                arg2 = stack.pop()
+                arg1 = stack.pop()
+                function = ops[token]
+                #print("here1\n")
+                result = function(arg1,arg2)
+                #print("here2\n")
+                stack.append(result)
+
+            except ArithmeticError:
+                print("Improper Arithmetic")
+                return -1
+
+            except:
+                print("Improper use of calculator")
+                return -1
+
+    print(stack)
     return stack.pop()
 
 def main():
--- test_rpn.py
+++ test_rpn.py
@@ -1,11 +1,44 @@
-import uittest
+import unittest
 import rpn
 
-class TestBasics(unittest.TestCase): 
+class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2,result)
-    
+
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
-        self.assertEqual(2,result)   
+        self.assertEqual(2,result)
+
+    def test_divide(self):
+        result = rpn.calculate("5 3 /")
+        self.assertEqual(5/3,result)
+
+    def test_mult(self):
+        result = rpn.calculate("3 9 *")
+        self.assertEqual(27,result)
+
+    def test_mult(self):
+        result = rpn.calculate("3 0 *")
+        self.assertEqual(0,result)
+
+    def test_div0(self):
+        result = rpn.calculate("1 0 /")
+        self.assertEqual(-1,result)
+
+    def test_bad_input0():
+        #bad input in 0 posistion
+        result =  rpn.calculate("q");
+        self.assertEqual(-1,result);
+
+    def test_bad_input1():
+        #bad input in 1 posistion
+        result =  rpn.calculate("1 r +");
+        self.assertEqual(-1,result);
+
+    def test_bad_input2():
+        #bad input in 2 posistion
+        result =  rpn.calculate("1 2 $");
+        self.assertEqual(-1,result);
+
+    print("ALL TESTS PASSED BUNDURU!!!!")
